622772686b88569aab9f3f70b591b67a0b02dff5
Ruben_fMg.dev <rfmgu18@gmail.com>
2026-04-16 22:27:46 -0400

    avances menores

--- scripts.js
+++ scripts.js
@@ -3,28 +3,30 @@ const startBtn = document.getElementById("startBtn");
 const pauseBtn = document.getElementById ("pauseBtn");
 const resetBtn = document.getElementById ("resetBtn");
 
-let tiempo = 25 * 60;
+let tiempoTrabajo = 25 * 60;
+let tiempoDescanso = 5 * 60;
+let Tiempoactual = tiempoTrabajo
 let intervalo = null;
 
 function formatoMinsSecs(segundos) {
     const min = Math.floor(segundos / 60);
     const seg = segundos % 60;
+    return `${min.toString().padStart(2, "0")}:${seg.toString().padStart(2, "0")}`;
+}
+
+function actuDisplay() {
+  display.textContent = formatoMinsSecs(tiempoActual);
 }
 
 
 
-function iniciarTimer() {
-    if (intervalo) return;
-    intervalo = setInterval(() => {
+
+function iniciarTimer() {}
         if (tiempo <= 0) {
-            clearInterval(intervalo);
-            intervalo = null;
+
             alert("Haz trabajado mucho! Tómate un descanso :D");
         }
-        tiempo--;
-        actuDisplay();
-        }, 1000);
-}
+
 
 function pausarTimer() {
     intervalo = null;
@@ -33,10 +35,11 @@ function pausarTimer() {
 }
 
 function reiniciarTimer() {
+    intervalo = null
     pausarTimer();
     tiempo = 25 * 60;
     actuDisplay();
-    console.log(alert("Haz reiniciado el timer exitosamente!"));
+    alert("Haz reiniciado el timer exitosamente!");
     
 }
 
